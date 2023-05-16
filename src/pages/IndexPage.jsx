2b9e0f21f9d70148e3b479520f520d877437b8c7
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-16 16:42:41 +0530

    logout feature added :)

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../css/IndexPage.css";
 import Navbar from "../components/Navbar";
+import Cookies from "universal-cookie";
+import { useUserStore } from "../store/UserStore";
+import { useAuthStore } from "../store/AuthStore";
+
+const cookies = new Cookies();
+const token = cookies.get("TOKEN");
 
 function IndexPage() {
+  const fetchUserData = useUserStore((state) => state.userData);
+  const user = useUserStore((state) => state.user);
+  const logout = useAuthStore((state) => state.logout);
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
     <div className="index__page">
       <Navbar />
+      <button className="logout__button" type="submit" onClick={() => logout()}>
+        Logout
+      </button>
     </div>
   );
 }
