1b69137f8b1149f1c08030660ea532a6afa509e7
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-22 22:23:19 +0530

    urls container added

---
+++ src/components/UrlsContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {AiFillDelete} from "react-icons/ai"
+import {RxClipboardCopy} from "react-icons/rx"
+
+function UrlsContainer(props) {
+
+    const {url} = props
+
+  return (
+    <div key={url._id} className='urls__container' >
+        <div>{url.longUrl}</div>
+        <div>{window.location.hostname}/{url.shortUrl}</div>
+        <div className='urls__c__actions'>
+            <AiFillDelete className='action__del'/>
+            <RxClipboardCopy className='action__copy'/>
+        </div>
+    </div>
+  )
+}
+
+export default UrlsContainer
--- src/pages/UrlsPage.jsx
+++ src/pages/UrlsPage.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "../css/UrlsPage.css";
 import { useUrlStore } from "../store/UrlStore";
 import { Toaster, toast } from "react-hot-toast";
+import UrlsContainer from "../components/UrlsContainer";
 
 function UrlsPage() {
   const fetchAllUrls = useUrlStore((state) => state.fetchAllUrls);
@@ -12,12 +13,21 @@ function UrlsPage() {
   }, []);
 
   return (
-    <div className="url__page">
+    <div className="urls__page">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
-      <div>
-        {allUrls?.allUrls.map((it) => (
-          <p key={it._id}>{it.shortUrl}</p>
-        ))}
+
+      <div className="urls__page__content">
+        <div>
+          <h2>Long Urls</h2>
+          <h2>Short Urls</h2>
+          <h2>Actions</h2>
+        </div>
+
+        <div>
+          {allUrls?.allUrls.map((it) => (
+            <UrlsContainer url={it} />
+          ))}
+        </div>
       </div>
     </div>
   );
