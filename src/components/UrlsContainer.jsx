03aac8ece9d6f47dd495c5f95cee4216e5837aae
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-23 20:25:02 +0530

    copy function impemented :D

--- src/components/UrlsContainer.jsx
+++ src/components/UrlsContainer.jsx
@@ -3,12 +3,18 @@ import "../css/UrlsContainer.css";
 import { AiFillDelete } from "react-icons/ai";
 import { RxClipboardCopy } from "react-icons/rx";
 import { Toaster, toast } from "react-hot-toast";
+import copy from "copy-to-clipboard";
 
 function UrlsContainer(props) {
   const { url } = props;
 
   const truncate = function (string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  };
+
+  const handlecopy = function () {
+    copy(`${window.location.hostname}/${url?.shortUrl}`);
+    toast.success("Short URL Copied to Clipboard! 📋");
   };
 
   return (
@@ -20,7 +26,7 @@ function UrlsContainer(props) {
       </div>
       <div className="urls__c__actions">
         <AiFillDelete className="action" title="Delete" />
-        <RxClipboardCopy className="action" title="Copy" />
+        <RxClipboardCopy className="action" title="Copy" onClick={handlecopy} />
       </div>
     </div>
   );
