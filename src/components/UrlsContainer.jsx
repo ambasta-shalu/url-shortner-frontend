23a63b548c360d14687f2b6be8ae20cf17f73611
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-23 15:24:01 +0530

    urls layout added

--- src/components/UrlsContainer.jsx
+++ src/components/UrlsContainer.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import {AiFillDelete} from "react-icons/ai"
-import {RxClipboardCopy} from "react-icons/rx"
+import React from "react";
+import "../css/UrlsContainer.css";
+import { AiFillDelete } from "react-icons/ai";
+import { RxClipboardCopy } from "react-icons/rx";
 
 function UrlsContainer(props) {
-
-    const {url} = props
+  const { url } = props;
 
   return (
-    <div key={url._id} className='urls__container' >
-        <div>{url.longUrl}</div>
-        <div>{window.location.hostname}/{url.shortUrl}</div>
-        <div className='urls__c__actions'>
-            <AiFillDelete className='action__del'/>
-            <RxClipboardCopy className='action__copy'/>
-        </div>
+    <div key={url._id} className="urls__container">
+      <div>{url.longUrl}</div>
+      <div>
+        {window.location.hostname}/{url.shortUrl}
+      </div>
+      <div className="urls__c__actions">
+        <AiFillDelete className="action__del" />
+        <RxClipboardCopy className="action__copy" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default UrlsContainer
+export default UrlsContainer;
