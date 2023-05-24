d45f4fb1c47dd30516c04482630a30b28d51f6d0
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-24 12:37:03 +0530

    layout fixed for urls of len 0

--- src/pages/UrlsPage.jsx
+++ src/pages/UrlsPage.jsx
@@ -16,19 +16,23 @@ function UrlsPage() {
     <div className="urls__page">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
 
-      <div className="urls__page__content">
-        <div className="urls__page__header">
-          <h2>Long Url</h2>
-          <h2>Short Url</h2>
-          <h2>Action</h2>
-        </div>
+      {allUrls?.allUrls.length ? (
+        <div className="urls__page__content">
+          <div className="urls__page__header">
+            <h2>Long Url</h2>
+            <h2>Short Url</h2>
+            <h2>Action</h2>
+          </div>
 
-        <div>
-          {allUrls?.allUrls.map((it) => (
-            <UrlsContainer url={it} />
-          ))}
+          <div>
+            {allUrls?.allUrls.map((it) => (
+              <UrlsContainer url={it} />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <h1 className="urls__page__msg">You have no URLs 😷</h1>
+      )}
     </div>
   );
 }
