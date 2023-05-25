69d0d9c59e72263f784ef38ba059eff2afd686be
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-25 19:19:39 +0530

    server domain replaced by local host

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const REACT_APP_SERVER_DOMAIN =
-  "https://url-shortner-backend-pkzp.onrender.com";
+// export const REACT_APP_SERVER_DOMAIN =
+//   "https://url-shortner-backend-pkzp.onrender.com";
 
-// export const REACT_APP_SERVER_DOMAIN = "http://localhost:7000";
+export const REACT_APP_SERVER_DOMAIN = "http://localhost:7000";
