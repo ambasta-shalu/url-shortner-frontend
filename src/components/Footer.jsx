39bf4687fd6ef9c0e7761fafb1acca01c5de3d89
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-27 13:54:31 +0530

    footer issues fixed :)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import "../css/Footer.css";
 
 function Footer() {
+  const handleClick = function () {
+    window.open("https://shalu-ambasta.vercel.app/", "_blank", "noopener");
+  };
+
   return (
     <footer className="footer">
       <div className="footer__content">
         <p>
           Coded with ReactJS{" "}
-          <i className="f__text f__heart__symbol">&#10084;</i>
-          by <span className="f__text dev__name nowrap"> Shalu Ambasta</span>
+          <i className="f__text f__heart__symbol">&#10084;</i> by{" "}
+          <span className="f__text dev__name" onClick={handleClick}>
+            Shalu Ambasta
+          </span>
         </p>
       </div>
     </footer>
--- src/pages/UrlsPage.jsx
+++ src/pages/UrlsPage.jsx
@@ -5,6 +5,7 @@ import { Toaster, toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import UrlsContainer from "../components/UrlsContainer";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function UrlsPage() {
   const navigate = useNavigate();
@@ -45,6 +46,7 @@ function UrlsPage() {
       ) : (
         <h1 className="urls__page__msg">You have No URLs 😷</h1>
       )}
+      <Footer />
     </div>
   );
 }
