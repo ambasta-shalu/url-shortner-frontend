6b87f84c8100343cc663ece223dcb8fa96d29c7c
Shalu Ambasta <itsambastakashish@gmail.com>
2023-05-30 14:00:03 +0530

    logo connected to navogation

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "../css/Logo.css";
+import { useNavigate } from "react-router-dom";
 
 function Logo() {
+  const navigate = useNavigate();
+
+  const handleClick = function () {
+    navigate("/");
+  };
+
   return (
     <div className="logo">
-      <h1 className="logo__title nowrap">URL Shortner</h1>
+      <h1 className="logo__title nowrap" onClick={handleClick}>
+        URL Shortner
+      </h1>
     </div>
   );
 }
